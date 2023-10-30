fee2815dab8788d4c19160833c2380d529a32bf7
FatimahSyed27 <89406364+FatimahSyed27@users.noreply.github.com>
2023-10-29 23:59:26 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,29 +1,30 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     const tipForm = document.getElementById("tipForm");
     const billTotal = document.getElementById("billTotal");
     const tipPercentage = document.getElementById("tipPercentage");
+    const tipSlider = document.getElementById("tipSlider");
     const tipAmount = document.getElementById("tipAmount");
     const totalWithTip = document.getElementById("totalWithTip");
-    const tipSlider = document.getElementById("tipSlider");
 
-    tipForm.addEventListener("input", calculateTip);
-    tipSlider.addEventListener("change", calculateTip);
+    tipForm.addEventListener("input", updateTip);
 
-    function calculateTip() {
-        const bill = parseFloat(billTotal.value);
-        const tipPercent = tipSlider.value;
-        const tip = (bill * tipPercent / 100).toFixed(2);
-        const total = (bill + parseFloat(tip)).toFixed(2);
-
-        if (isNaN(bill)) {
-            tipAmount.value = "Invalid Input";
-            totalWithTip.value = "Invalid Input";
-        } else {
-            tipPercentage.value = tipPercent + "%";
-            tipAmount.value = tip;
-            totalWithTip.value = total;
+    function updateTip() {
+        const billValue = parseFloat(billTotal.value);
+        const tipValue = parseFloat(tipSlider.value);
+        
+        if (isNaN(billValue)) {
+            alert("Please enter a valid number for Bill Total.");
+            return;
         }
+
+        const tipPercentageValue = tipValue + "%";
+        tipPercentage.value = tipPercentageValue;
+
+        const tipAmountValue = (billValue * tipValue / 100).toFixed(2);
+        tipAmount.value = tipAmountValue;
+
+        const totalWithTipValue = (billValue + parseFloat(tipAmountValue)).toFixed(2);
+        totalWithTip.value = totalWithTipValue;
     }
 });
-
 
