d3a2cd48ed8ba1d8ff4f4523258d9b88e2065b50
FatimahSyed27 <89406364+FatimahSyed27@users.noreply.github.com>
2023-10-30 01:01:49 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,21 +1,30 @@
-const billTotal = document.getElementById('billTotal');
-const tipSlider = document.getElementById('tipSlider');
-const tipPercentage = document.getElementById('tipPercentage');
-const tipAmount = document.getElementById('tipAmount');
-const totalWithTip = document.getElementById('totalWithTip');
+document.addEventListener("DOMContentLoaded", function () {
+    const tipForm = document.getElementById("tipForm");
+    const billTotal = document.getElementById("billTotal");
+    const tipPercentage = document.getElementById("tipPercentage");
+    const tipSlider = document.getElementById("tipSlider");
+    const tipAmount = document.getElementById("tipAmount");
+    const totalWithTip = document.getElementById("totalWithTip");
 
-document.getElementById('tipForm').addEventListener('input', function() {
-    let bill = parseFloat(billTotal.value);
-    let tip = parseFloat(tipSlider.value);
-    let tipValue = (bill * tip) / 100;
-    let total = bill + tipValue;
+    tipForm.addEventListener("input", updateTip);
 
-    if (isNaN(bill)) {
-        alert("Please enter a valid bill amount.");
-        return;
+    function updateTip() {
+        const billValue = parseFloat(billTotal.value);
+        const tipValue = parseFloat(tipSlider.value);
+        
+        if (isNaN(billValue)) {
+            alert("Please enter a valid number for Bill Total.");
+            return;
+        }
+
+        const tipPercentageValue = tipValue + "%";
+        tipPercentage.value = tipPercentageValue;
+
+        const tipAmountValue = (billValue * tipValue / 100).toFixed(2);
+        tipAmount.value = tipAmountValue;
+
+        const totalWithTipValue = (billValue + parseFloat(tipAmountValue)).toFixed(2);
+        totalWithTip.value = totalWithTipValue;
     }
-
-    tipPercentage.textContent = tip + "%";
-    tipAmount.value = tipValue.toFixed(2);
-    totalWithTip.value = total.toFixed(2);
 });
+
