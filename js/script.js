f11fcbe325e65b8870e7c2ca3ade24a038b24606
FatimahSyed27 <89406364+FatimahSyed27@users.noreply.github.com>
2023-10-29 22:45:59 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const tForm = document.getElementById("tForm");
+    const fullTotal = document.getElementById("fullTotal");
+    const tipPer = document.getElementById("tipPer");
+    const tipSli = document.getElementById("tipSli");
+    const tipAmount = document.getElementById("tipAmount");
+    const totalBillWithTip = document.getElementById("totalBillWithTip");
+
+    tForm.addEventListener("input", updateTipAmount);
+
+    function updateTipAmount() {
+        const bill = parseFloat(fullTotal.value);
+        const tipPercent = tipSli.value;
+        const tip = (bill * tipPercent) / 100;
+        const total = bill + tip;
+
+        if (isNaN(bill)) {
+            tipAmount.value = "Invalid Input";
+            totalBillWithTip.value = "Invalid Input";
+        } else {
+            tipAmount.value = tip.toFixed(2);
+            totalBillWithTip.value = total.toFixed(2);
+            tipPer.value = tipPercent + "%";
+        }
+    }
+});
 
